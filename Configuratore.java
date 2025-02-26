2892a8a9e15442538f392cbfc7887deec5621aa7
andreaZappini <zappini.andrea03@gmail.com>
2025-02-26 11:12:03 +0100

    modifiche a alcune classi

--- Configuratore.java
+++ Configuratore.java
@@ -3,4 +3,8 @@ public class Configuratore extends Utente{
     public Configuratore(Credenziale credenziali){
         super(credenziali);
     }
+
+    protected void modificaPassword(String nuovaPassword){
+        this.getCredenziali().setPassword(nuovaPassword);
+    }   
 }
--- Main.java
+++ Main.java
@@ -9,21 +9,43 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class Main {
+    private static boolean abilitazioneModificaCorpoDati = true;
     public static void main(String[] args) {
 
-        File file = new File("configuratore.xml");
+        ElencoUtenti elencoUtenti = new ElencoUtenti();
+
         System.out.println("Benvenuto configuratore!");
         System.out.println("Inserisci username e password per accedere");
+        if(abilitazioneModificaCorpoDati){
+            File file = new File("configuratore1.xml");
+            login(file, elencoUtenti);
+        }
+
+    }
+
+    private static void login(File file, ElencoUtenti elencoUtenti) {
         try(Scanner in = new Scanner(System.in)){
             System.out.println("username:");
             String username = in.next();
             System.out.println("password:");
             String password = in.next();
-            leggiXML(file, username, password);
+            Credenziale credenziali = new Credenziale(username, password);
+            Configuratore configuratore = new Configuratore(credenziali);
+            elencoUtenti.aggiungiUtente(configuratore);
+            if(leggiXML(file, username, password)){
+                System.out.println("Accesso consentito");
+                System.out.println("necessiario modificare credenziali");
+                System.out.println("Inserisci nuova password:");
+                String nuovaPassword = in.next();
+                elencoUtenti.ge
+                
+            }else{
+                System.out.println("Accesso negato");
+                login(file, elencoUtenti);
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
-
     }
 
     private static boolean leggiXML(File file, String username, String password) throws Exception{
@@ -31,17 +53,27 @@ public class Main {
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document doc = builder.parse(file);
 
-        NodeList lista = doc.getElementsByTagName("persona");
+        NodeList lista = doc.getElementsByTagName("configuratore");
+        String u = null;
+        String psw = null;
+
         for(int i = 0; i < lista.getLength(); i++){
             Node p = lista.item(i);
             if(p.getNodeType() == Node.ELEMENT_NODE){
                 Element persona = (Element) p;
-                System.out.println("Nome: " + persona.getElementsByTagName("nome").item(0).getTextContent());
-                System.out.println("Cognome: " + persona.getElementsByTagName("cognome").item(0).getTextContent());
-                System.out.println("Eta: " + persona.getElementsByTagName("eta").item(0).getTextContent());
+                u = persona.getElementsByTagName("username").item(0).getTextContent();
+                psw = persona.getElementsByTagName("password").item(0).getTextContent();
+                System.out.println("username: " + u);
+                System.out.println("password: " + psw);
             }
-
         }
+        if(u.equals(username) && psw.equals(password)){
+            System.out.println("Accesso consentito");
+            //effettua modifiche corpo dati
+             
+            return true;
+        }
+        return false;
     }
 }
 
